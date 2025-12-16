8e7ed11f40d2104703daa732f20b51fcfce0270d
DuckWithACrowbar <8024382@student.harmonytx.org>
2025-12-16 08:44:41 -0600

    DUCKS!!!

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+let name = prompt("What is your name?");
+alert("Hello " + name + "!");
+alert("I am a duck!")
+alert("More specifically,");
+alert("a duck with a crowbar...");
+alert("Anyways, here's some ducks!");
