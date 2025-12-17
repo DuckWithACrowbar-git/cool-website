9976e00d6b2bad4371e00b6dc5a32648675db846
DuckWithACrowbar <8024382@student.harmonytx.org>
2025-12-17 08:44:12 -0600

    a

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("What are you doing in the console?")
+console.log("Wondering how this website works, huh?")
+console.log("Reach out to me at @DuckWithACrowbar on Instagram or @DuckWithAGun on X!")
