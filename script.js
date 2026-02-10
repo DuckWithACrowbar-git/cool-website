b5081715a6397f3751f09173b08470f0aa86543e
DuckWithACrowbar-git <8024382@student.harmonytx.org>
2026-02-09 18:54:06 -0600

    This is turning into a Linux info page

--- script.js
+++
@@ -1,3 +0,0 @@
-console.log("What are you doing in the console?")
-console.log("Wondering how this website works, huh?")
-console.log("Reach out to me at @DuckWithACrowbar on Instagram or @DuckWithAGun on X!")
