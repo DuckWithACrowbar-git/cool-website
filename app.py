cd5fa44eb209f171e42861b8e2a90ca2b5027c36
Hussein Khaleel <8024382@student.harmonytx.org>
2026-07-24 07:36:55 -0500

    Now its hexadecimal color codes, not numbers :D

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, redirect
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import random as r
-import requests
-
-URL = "http://localhost:5000"
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["5 per second"],
+    storage_uri="memory://",
+)
+
+
 @app.route("/")
 def root():
-    # response = requests.get(URL+"/random").text
-    return render_template('index.html')
+    result_color = ["#"]
+    for i in range(6):
+        for j in range(3):
+            choice = r.choice(values)
+        result_color.append(choice)
+    result_color = "".join(result_color)
+    return render_template('index.html', color=result_color), 200
 
-@app.route("/random", methods=['GET'])
-def random():
-    for i in range(10):
-        rand = r.random()
-    rand = str(rand)
-    rand = rand.replace("0.", "")
-    rand = int(rand)
-    return jsonify(rand), 200
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect("/"), 308
+
+@app.errorhandler(429)
+def ratelimit_handler(e):
+    return "Rate limit exceeded. Slow down."
 
 
 if __name__ == "__main__":
