b86c05bcab86f4832ff9232a4e8e544af3e99aed
Hussein Khaleel <8024382@student.harmonytx.org>
2026-07-28 15:06:23 -0500

    Finished, ready for copy to its own repo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, redirect, url_for
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from time import sleep
+from time import sleep, ctime
 import threading as t
 import requests
 
@@ -18,8 +18,10 @@ certainty = " "
 event = " "
 description = " "
 instruction = " "
+NWSheadline = " "
 
 def get_data():
+    sleep(5)
     while True:
         response = requests.get(URL)
         data = response.json()
@@ -33,6 +35,7 @@ def get_data():
         description = (data["features"][0]["properties"]["description"])
         instruction = (data["features"][0]["properties"]["instruction"])
         NWSheadline = (data["features"][0]["properties"]["parameters"]["NWSheadline"][0])
+        print("Pulled data at " + ctime())
         sleep(TIME_TO_WAIT)
 
 
@@ -60,6 +63,19 @@ def root():
 def about():
     return render_template('about.html'), 200
 
+@app.route("/api")
+def api():
+    value = jsonify({"alert_id": alert_id, 
+                     "sent_date": sent_date, 
+                     "expiry_date": expiry_date, 
+                     "severity": severity, 
+                     "certainty": certainty, 
+                     "event": event, 
+                     "description": description, 
+                     "instruction": instruction,
+                     "headline": NWSheadline})
+    return value, 200
+
 @app.errorhandler(429)
 def ratelimit(error):
     return render_template('ratelimit.html'), 200
@@ -71,4 +87,4 @@ def ratelimit(error):
 if __name__ == "__main__":
     thread = t.Thread(target=get_data, daemon=True)
     thread.start()
-    app.run(host="0.0.0.0")
+    app.run()
